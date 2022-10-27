791e68ce921cdece29a531429c9688652c4a012d
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-27 10:35:03 -0300

    👌 IMPROVE: Make Changes

--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
-# if __name__ == '__main__':
-    
+from fastapi import HTTPException, status
+from fastapi import Response
+from fastapi import FastAPI
+
+if __name__ == '__main__':
+    import uvicorn
--- src/modules/functions/Request.py
+++
@@ -1,4 +0,0 @@
-class Request:
-    def __init__(self):
-        from fastapi import FastAPI
-        self.app = FastAPI()
